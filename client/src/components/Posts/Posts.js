0cc0b017fe241059dde74ce2b007f2e8129fd2a2
Omar Mahdy <omar.mahdy246@gmail.com>
2023-03-10 10:38:36 +0800

    Fetch all action added - Building the UI of the form component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Post from "./Post/Post";
+import { useSelector } from "react-redux";
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  console.log("These are the posts", posts);
   return (
     <>
       <Post />
