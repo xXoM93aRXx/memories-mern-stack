95786d2a7a0eddd97bebb2989258a9cf3b84f6dc
Omar Mahdy <omar.mahdy246@gmail.com>
2023-03-10 09:54:40 +0800

    Created components and styling - Starting creating Api and Redux boilerplate

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "./Post/Post";
+const Posts = () => {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+};
+export default Posts;
